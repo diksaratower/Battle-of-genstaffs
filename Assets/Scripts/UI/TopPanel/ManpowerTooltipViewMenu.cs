2e8b776cb23b58ae381507835c874e23127e4289
Вася <77069617+diksaratower@users.noreply.github.com>
2024-01-13 15:50:00 +0400

    Пофиксил тултип менпавера

--- Assets/Scripts/UI/TopPanel/ManpowerTooltipViewMenu.cs
+++ Assets/Scripts/UI/TopPanel/ManpowerTooltipViewMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 
@@ -5,11 +6,26 @@ public class ManpowerTooltipViewMenu : TooltipViewMenu
 {
     public override void RefreshUI(TooltipHandlerUI tooltipHandler)
     {
-        AddSimpleText($"Резерв: {ToMillions(((float)Player.CurrentCountry.CountryPreset.Population / 100f) * 5f)}млн", false);
+        AddSimpleText($"Резерв: {ToPettyNumberView(Player.CurrentCountry.EquipmentStorage.GetEquipmentCount(EquipmentType.Manpower))}", false);
         
-        AddSimpleText($"{5f}% от общего населения страны: {ToMillions(Player.CurrentCountry.CountryPreset.Population)}млн", false);
+        var conscriptionPercent = Player.CurrentCountry.Politics.GetConscriptionPercent();
+        AddSimpleText($"{conscriptionPercent}% от общего населения страны: {ToPettyNumberView(Player.CurrentCountry.CountryPreset.Population)}", false);
         AddSimpleText($"А ведь каждого солдата есть семья...", false);
         base.RefreshUI(tooltipHandler);
+    }
+
+    private string ToPettyNumberView(float value)
+    {
+        var result = Math.Round(value, 2).ToString();
+        if (value > 1000f)
+        {
+            result = Math.Round(value / 1000f).ToString() + " тыс.";
+        }
+        if (value > 1000000f)
+        {
+            result = Math.Round(value / 1000000f).ToString() + " млн.";
+        }
+        return result;
     }
 
     private float ToMillions(float value)
